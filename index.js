30979709462401774efb8a65f76124119245b5b3
Dave Arata <darata@thecommsapp.com>
2015-05-20 11:53:16 -0400

    commented console logs and added version number to package.json

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ var _ = require('lodash');
 var connections = {};
 
 var connect = function connect(uri) {
-  console.log('creating mongoose connection to: ' + uri);
+  //console.log('creating mongoose connection to: ' + uri);
   var connection = mongoose.createConnection(uri);
 
 
   connection.on('connected', function () {
-    console.log('Mongoose connection open to  ' + uri);
+    //console.log('Mongoose connection open to  ' + uri);
   });
 
   // When the connection is disconnected
   connection.on('disconnected', function () {
-    console.log('Mongoose ' + uri + ' connection disconnected');
+    //console.log('Mongoose ' + uri + ' connection disconnected');
   });
 
   // If the Node process ends, close the Mongoose connection
   process.on('SIGINT', function () {
     connection.close(function () {
-      console.log(uri + ' connection disconnected through app termination');
+      //console.log(uri + ' connection disconnected through app termination');
       process.exit(0);
     });
   });

d58053cc0aa555da4ea5852280111505217208c8
Dave Arata <darata@thecommsapp.com>
2015-06-03 11:10:01 -0400

    changed logic for getModel to work more consistent

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ var connect = function connect(uri) {
   var connection = mongoose.createConnection(uri);
 
 
-  connection.on('connected', function() {
+  connection.on('connected', function () {
     //console.log('Mongoose connection open to  ' + uri);
   });
 
   // When the connection is disconnected
-  connection.on('disconnected', function() {
+  connection.on('disconnected', function () {
     //console.log('Mongoose ' + uri + ' connection disconnected');
   });
 
   // If the Node process ends, close the Mongoose connection
-  process.on('SIGINT', function() {
-    connection.close(function() {
+  process.on('SIGINT', function () {
+    connection.close(function () {
       //console.log(uri + ' connection disconnected through app termination');
       process.exit(0);
     });
@@ -45,8 +45,7 @@ module.exports = {
 
   getModel: function getModel(connection, name, schema) {
     var modelNames = connection.modelNames();
-    var modelName = _.find(modelNames, name);
-    if (_.isString(modelName)) {
+    if (_.includes(modelNames, name)) {
       return connection.model(name);
     } else {
       return connection.model(name, schema);
